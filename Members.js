bae2704bd1776c1f89950ade18675cd27e8db94b
Administrator <administrator@CN-7.lan>
2019-05-03 12:27:55 +0100

    express blog

---
+++ Members.js
@@ -0,0 +1,21 @@
+let members = [{
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    status: 'active'
+},
+{
+    id: 2,
+    name: 'Jane Doe',
+    email: 'jane@gmail.com',
+    status: 'inactive'
+},
+{
+    id: 3,
+    name: 'Philip Doe',
+    email: 'philip@gmail.com',
+    status: 'active'
+}
+];
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger');
+
+const app = express();
+
+// Init middleware
+app.use(logger);
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API routes
+app.use('/api/members', require('./routes/api/members'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+
+// Express Format
+// app.get('/', function(req, res){
+//     //fetch from db
+//     //load pages
+//     //return json
+//     //full access to req and res
+// })
